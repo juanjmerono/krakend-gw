59431b4155b5ebe168e55f8a5fda117a10b1b870
juanjmerono <jjmerono@um.es>
2021-11-16 01:43:16 +0100

    Remote client try

--- oidc-api/src/main/java/com/thehecklers/ssecressvr/SsecResSvrApplication.java
+++ oidc-api/src/main/java/com/thehecklers/ssecressvr/SsecResSvrApplication.java
@@ -4,18 +4,35 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrations;
+import org.springframework.security.oauth2.client.registration.InMemoryReactiveClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction;
+import org.springframework.security.oauth2.client.web.server.UnAuthenticatedServerOAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 public class SsecResSvrApplication {
@@ -29,8 +46,49 @@ public class SsecResSvrApplication {
 @Configuration
 class JWTSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private static final Logger log = LoggerFactory.getLogger(JWTSecurityConfig.class);
+
+    @Bean("rcr")
+    ReactiveClientRegistrationRepository getRegistration(
+            @Value("${spring.security.oauth2.client.registration.casum.client-id}") String clientId,
+            @Value("${spring.security.oauth2.client.registration.casum.client-secret}") String clientSecret,
+            @Value("${spring.security.oauth2.client.provider.casum.issuer-uri}") String issuerUri
+    ) {
+        ClientRegistration registration = ClientRegistrations.fromIssuerLocation(issuerUri)
+                .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+                .registrationId("casum")
+                .clientId(clientId)
+                .clientSecret(clientSecret)
+                .clientAuthenticationMethod(ClientAuthenticationMethod.POST)
+                .build();
+
+        return new InMemoryReactiveClientRegistrationRepository(registration);
+    }
+    @Bean(name = "client")
+    WebClient webClient(@Qualifier("rcr") ReactiveClientRegistrationRepository clientRegistrations) {
+        ServerOAuth2AuthorizedClientExchangeFilterFunction oauth = 
+            new ServerOAuth2AuthorizedClientExchangeFilterFunction(clientRegistrations, 
+                                    new UnAuthenticatedServerOAuth2AuthorizedClientRepository());
+        oauth.setDefaultClientRegistrationId("casum");
+        return WebClient.builder()
+                .filter(oauth)
+                .filter(logRequest())
+                .build();
+    }
+
+    private ExchangeFilterFunction logRequest() {
+        return ExchangeFilterFunction.ofRequestProcessor(clientRequest -> {
+            log.info("Request: [{}] {}", clientRequest.method(), clientRequest.url());
+            log.debug("Payload: {}", clientRequest.body());
+
+            return Mono.just(clientRequest);
+        });
+    }
+        
     @Override
     protected void configure(HttpSecurity http) throws Exception {// @formatter:off
+        http
+            .oauth2Client();
         http
             .authorizeRequests()
 			.mvcMatchers("/public/**").permitAll()
@@ -38,7 +96,7 @@ class JWTSecurityConfig extends WebSecurityConfigurerAdapter {
             .anyRequest()
             .authenticated()
             .and()
-            .oauth2ResourceServer().jwt();          
+            .oauth2ResourceServer().jwt();
 	}
 }
 
@@ -59,6 +117,11 @@ class PublicResourceController {
 @RestController
 @RequestMapping("/oauth")
 class ResourceController {
+	private final WebClient client;
+
+	public ResourceController(WebClient client) {
+		this.client = client;
+	}
 
     @Value("${SNAME}")
     private String sName;
@@ -75,5 +138,18 @@ class ResourceController {
         hmap.put("sub",principal.getSubject());
         return hmap;
     }
+
+	@GetMapping("/remote/{server}/{method}")
+	String getFromServer(@PathVariable("server") String server,
+						 @PathVariable("method") String method) {
+        
+        String rootPath = "__debug".equals(method)?"__debug":"oauth";
+        String finalServer = "__debug".equals(method)?"apigateway":server;
+		return client.get()
+				.uri("http://"+finalServer+":8080/"+rootPath+"/"+method)
+				.retrieve()
+				.bodyToMono(String.class)
+				.block();
+	}
 
 }
